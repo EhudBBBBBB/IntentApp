c46743ce4531780c9d42c727493228f8e25a5ef9
ehud <ehudis2016@gmail.com>
2025-03-03 16:00:12 +0200

    IntentApp- everything working fantastic.

--- app/src/main/java/com/example/intentapp/MainActivity.java
+++ app/src/main/java/com/example/intentapp/MainActivity.java
@@ -1,18 +1,78 @@
 package com.example.intentapp;
+import static androidx.activity.result.ActivityResultCallerKt.registerForActivityResult;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    GoogleSignInClient googleSignInClient;
+    ShapeableImageView imageView;
+    TextView nameTv, mailTv;
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult o) {
+                    if(o.getResultCode() == RESULT_OK){
+                        Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(o.getData());;
+                        try {
+                            GoogleSignInAccount account = accountTask.getResult(ApiException.class);
+                            AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+                            auth.signInWithCredential(credential).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(Task<AuthResult> task) {
+                                    if(task.isSuccessful()){
+                                        Glide.with(MainActivity.this).load(Objects.requireNonNull(auth.getCurrentUser()).getPhotoUrl()).into(imageView);
+                                        nameTv.setText(auth.getCurrentUser().getDisplayName());
+                                        mailTv.setText(auth.getCurrentUser().getEmail());
+                                        Toast.makeText(MainActivity.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                                    }else{
+                                        Toast.makeText(MainActivity.this, "failed to Logged in "+task.getException(), Toast.LENGTH_SHORT).show();
+                                    }
+                                    }
+
+                            });
+                        } catch (ApiException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                }
+            }
+
+    )
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,22 +81,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Button openBrowserButton = findViewById(R.id.openBrowserButton);
-        Button makeCallButton = findViewById(R.id.makeCallButton);
-        Button shareTextButton = findViewById(R.id.shareTextButton);
-
-        openBrowserButton.setOnClickListener(v -> {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
-            startActivity(browserIntent);
-        });
-        makeCallButton.setOnClickListener(v -> {
-            Intent callIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:123456789"));
-            startActivity(callIntent);
-        });
-        shareTextButton.setOnClickListener(v->{
-            Intent intent = new Intent(MainActivity.this, ShareActivity.class);
-            startActivity(intent);
-        });
     }
+
 }
 
